b818ce5a8d9c57af4b5aa055150bb9b9aab2b4de
Christoffer Olander <christoffer@pingpong.net>
2015-11-17 14:23:01 +0100

    added documents-sample

--- check.sh
+++ check.sh
@@ -11,3 +11,5 @@ for betygapiversion in r01 r10; do
 	validate_xml betyg/$betygapiversion/grades.xsd betyg/$betygapiversion/grades-sample.xml
 	validate_xml betyg/$betygapiversion/grade-documents.xsd betyg/$betygapiversion/grade-documents-sample.xml
 done
+
+validate_xml dokument/r10/documents.xsd dokument/r10/documents-sample.xml
