44d4eb2c18cd835ef4aa5abf435de21a75d1aa63
Fredrik Fornwall <fredrik@fornwall.net>
2015-11-18 15:36:40 +0100

    Remove repetition from check.sh

--- check.sh
+++ check.sh
@@ -9,10 +9,7 @@ validate_xml () {
 
 validate_xml betyg/r01/grades.xsd betyg/r01/grades-sample.xml
 validate_xml betyg/r01/grade-documents.xsd betyg/r01/grade-documents-sample.xml
-validate_xml betyg/r10/betyg.xsd betyg/r10/betyg-exempel.xml
 
-validate_xml betygsdokument/r10/betygsdokument.xsd betygsdokument/r10/betygsdokument-exempel.xml
-
-validate_xml styrdokument/r10/styrdokument.xsd styrdokument/r10/styrdokument-exempel.xml
-
-validate_xml nationellaprov/r10/nationellaprov.xsd nationellaprov/r10/nationellaprov-exempel.xml
+for function in betyg betygsdokument styrdokument nationellaprov; do
+	validate_xml $function/r10/${function}.xsd $function/r10/${function}-exempel.xml
+done
