4550d0d4a0169bb5956819cdc2b7c50fce96c2c3
Fredrik Fornwall <fredrik@fornwall.net>
2016-08-16 10:38:36 +0200

    Remove old r01 API

--- check.sh
+++ check.sh
@@ -7,9 +7,6 @@ validate_xml () {
 	xmllint --noout --schema $1 $2
 }
 
-validate_xml betyg/r01/grades.xsd betyg/r01/grades-sample.xml
-validate_xml betyg/r01/grade-documents.xsd betyg/r01/grade-documents-sample.xml
-
 for function in betyg betygsdokument styrdokument nationellaprov; do
 	validate_xml $function/r10/${function}.xsd $function/r10/${function}-exempel.xml
 	validate_xml $function/r11/${function}.xsd $function/r11/${function}-exempel.xml
