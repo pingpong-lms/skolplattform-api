dc886bcf694327615a50d5ed90e95103d18a2f75
Fredrik Fornwall <fredrik@fornwall.net>
2017-05-05 12:29:42 +0200

    Avoid hardcoded versions in check.sh

--- check.sh
+++ check.sh
@@ -8,11 +8,9 @@ validate_xml () {
 }
 
 for function in betyg betygsdokument styrdokument nationellaprov; do
-	for version in r10 r11 r12; do
-		XSD_FILE=$function/$version/${function}.xsd
-		XML_FILE=$function/$version/${function}-exempel.xml
-		if [ -f $XSD_FILE ]; then
-			validate_xml $XSD_FILE $XML_FILE
-		fi
+	for version in $function/*; do
+		XSD_FILE=$version/${function}.xsd
+		XML_FILE=$version/${function}-exempel.xml
+		validate_xml $XSD_FILE $XML_FILE
 	done
 done
